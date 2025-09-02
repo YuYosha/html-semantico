4fb96bf1c7b67787c21982b02f2f8ef54d6cd8fb
YuYosha <eugenioram123@gmail.com>
2025-09-02 12:11:29 -0500

    whatafak

--- CSS/js/app.js
+++ CSS/js/app.js
@@ -1,12 +1,12 @@
 //var toggleBtn = document.getElementByld("theme-toggle-btn");
 //let toggleBtn = document.getElementByld("theme-toggle-btn");
 //const toggleBtn = document.getElementByld("theme-toggle-btn");
-//const PI = 3.14;
+//const PI = 3.14; 
 
 let toggleBtn = document.querySelector(".theme-toggle-btn");
 
 toggleBtn.addEventListener("click", function() {
-    
+
 alert("CLICK");
 
 });
