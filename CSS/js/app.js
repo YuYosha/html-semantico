e40674e5a4484eb89e7ebc84192b9915e0919142
YuYosha <eugenioram123@gmail.com>
2025-09-02 12:49:12 -0500

    icons 3

--- CSS/js/app.js
+++ CSS/js/app.js
@@ -3,11 +3,18 @@
 //const toggleBtn = document.getElementByld("theme-toggle-btn");
 //const PI = 3.14; 
 
-let toggleBtn = document.querySelector(".theme-toggle-btn");
+//let toggleBtn = document.querySelector(".theme-toggle-btn");
 
-toggleBtn.addEventListener("click", function() {
-document.body.classList.toggle("dark-mode");
+//toggleBtn.addEventListener("click", function() {
+document.body.classList.toggle('dark-mode');
+if (document.body.classList.contains('dark-moon')){
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+} else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+}
 //document.body.classList.
 //alert("CLICK YESSIR");
 
-});
+//});
