d9fa214d87f6009fe69c5f594931bd0ef4c3ce25
YuYosha <eugenioram123@gmail.com>
2025-09-02 12:58:04 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- CSS/js/app.js
+++ CSS/js/app.js
@@ -3,18 +3,21 @@
 //const toggleBtn = document.getElementByld("theme-toggle-btn");
 //const PI = 3.14; 
 
-//let toggleBtn = document.querySelector(".theme-toggle-btn");
+const toggleBtn = document.querySelector(".theme-toggle-btn");
+const icon = document.querySelector(".theme-toggle-btn i");
 
-//toggleBtn.addEventListener("click", function() {
-document.body.classList.toggle('dark-mode');
-if (document.body.classList.contains('dark-moon')){
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
     icon.classList.remove('fa-sun');
     icon.classList.add('fa-moon');
-} else {
+  } else {
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
+  }
 }
-//document.body.classList.
-//alert("CLICK YESSIR");
 
-//});
+toggleBtn.addEventListener("click", toggleTheme);
+    // Fixed: Removed misplaced code block and else statement
+    // (No code needed here, as the logic is already handled in toggleTheme function above. This block was erroneous.)
+//alert("CLICK YESSIR");
